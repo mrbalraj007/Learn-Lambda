ecc890c143ab891e8f2a859f4308e73b0acb07bc
Balraj Singh <53593772+mrbalraj007@users.noreply.github.com>
2025-05-20 19:52:23 +1000

    updateed code

--- 12.idle/build_openpyxl_layer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Build Openpyxl Lambda layer
-rm -rf python openpyxl_layer.zip
-mkdir -p python
-pip install openpyxl -t python
-zip -r openpyxl_layer.zip python
-echo "Created openpyxl_layer.zip"
